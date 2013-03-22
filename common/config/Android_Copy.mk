c9e580f7c0d3b7b11ac826b78cbc9cdf3d545e9a
Royce Jiang <jiangxd@embedinfo.com>
2013-03-22 18:48:21 -0400

    firmware: copy firmware files to target image if prebuild
    both wifi and bluetooth firmware will be copied.
    
    Signed-off-by: Royce Jiang <jiangxd@embedinfo.com>

--- common/config/Android_Copy.mk
+++ common/config/Android_Copy.mk
@@ -1,5 +1,5 @@
 # make file for new hardware  from 
-
+PREBUILD_FIRMWARE := false
 LOCAL_PATH := device/atmel/common/config
 
 # system configuration files
@@ -14,5 +14,10 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/atmel_touch_screen_controller.idc:system/usr/idc/atmel_touch_screen_controller.idc \
         $(LOCAL_PATH)/Atmel_maXTouch_Touchscreen.idc:system/usr/idc/Atmel_maXTouch_Touchscreen.idc     \
 	$(LOCAL_PATH)/wallpaper_info.xml:data/system/wallpaper_info.xml \
-	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml
 
+ifeq ($(PREBUILD_FIRMWARE), realtek)
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/wlan.ko:system/lib/modules/wlan.ko \
+	$(LOCAL_PATH)/rtk8723a.bin:system/etc/firmware/rtk8723a.bin
+endif
