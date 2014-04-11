5192ba1c7ee7b5f6518d4a47361d960960edd480
Royce Jiang <jiangxd@embedinfo.com>
2014-04-11 11:34:16 +0800

    build: update generic_no_telephony.mk to kitkat
    Signed-off-by: Royce Jiang <jiangxd@embedinfo.com>

--- common/generic_no_telephony.mk
+++ common/generic_no_telephony.mk
@@ -24,38 +24,41 @@ PRODUCT_PACKAGES := \
     Bluetooth \
     Calculator \
     Calendar \
+    Camera2 \
     CertInstaller \
-    DrmProvider \
     Email \
     Exchange2 \
     FusedLocation \
-    Gallery \
+    Gallery2 \
     InputDevices \
+    Keyguard \
     LatinIME \
     Launcher2 \
     Music \
     MusicFX \
+    OneTimeInitializer \
+    PrintSpooler \
     Provision \
-    Phone \
     QuickSearchBox \
     Settings \
     SystemUI \
+    TeleService \
     CalendarProvider \
     bluetooth-health \
     hostapd \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    WallpaperCropper
 
 PRODUCT_PACKAGES += \
     audio \
+    clatd \
+    clatd.conf \
     dhcpcd.conf \
     network \
     pand \
     pppd \
     sdptool \
     wpa_supplicant
-
-PRODUCT_PACKAGES += \
-    icu.dat
 
 PRODUCT_PACKAGES += \
     librs_jni \
@@ -81,10 +84,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.carrier=unknown
 
 $(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
-$(call inherit-product-if-exists, external/cibu-fonts/fonts.mk)
-$(call inherit-product-if-exists, external/lohit-fonts/fonts.mk)
+$(call inherit-product-if-exists, external/noto-fonts/fonts.mk)
 $(call inherit-product-if-exists, external/naver-fonts/fonts.mk)
+$(call inherit-product-if-exists, external/sil-fonts/fonts.mk)
 $(call inherit-product-if-exists, frameworks/base/data/keyboards/keyboards.mk)
+$(call inherit-product-if-exists, frameworks/webview/chromium/chromium.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
 
 # Overrides
