5833ac2a69f90adba56f9d71edce69fc96738a40
Royce Jiang <jiangxd@embedinfo.com>
2014-04-25 10:40:18 +0800

    ubifs: script change max leb size
    Signed-off-by: Royce Jiang <jiangxd@embedinfo.com>

--- release/Generate_ubifs_image/ubifs.sh
+++ release/Generate_ubifs_image/ubifs.sh
@@ -192,7 +192,7 @@ check_cmd "cp -ru $ANDROID_PRODUCT/$PRODUCT_DEVICE/data ./data"
 check_cmd "chmod 0777 -R ./data"
 
 if [ $BOARD_ID = "SAM9X5" ] || [ $BOARD_ID = "SAMA5D3" ] || [ $BOARD_ID = "SAMA5D3ISI" ]; then
-	check_cmd "mkfs.ubifs -x lzo -m 2KiB -e 124KiB -c 1000 -o system_ubifs.img -d system/"
+	check_cmd "mkfs.ubifs -m 2KiB -e 124KiB -c 1045 -o system_ubifs.img -d system/"
 	check_cmd "mkfs.ubifs -m 2KiB -e 124KiB -c 984 -o userdata_ubifs.img -d  data/"
 	check_cmd "mkfs.ubifs -m 2KiB -e 124KiB -c 1230 -o cache_ubifs.img -d  cache/"
 	check_cmd "ubinize -o ../$SYS_NAME -m 2KiB -p 128KiB -s 2048 ../system_ubi.cfg"
