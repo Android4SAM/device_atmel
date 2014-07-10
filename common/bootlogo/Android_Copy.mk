ef4a295fbab5e73709d1f80bba162d86278dffc9
Royce Jiang <jiangxd@embedinfo.com>
2014-07-10 11:28:04 +0800

    config: reconfig bootlogo load method
    it should set boot.lcd to wvga or hvga to load 800*480 and 480*272
    bootlogo
    
    Signed-off-by: Royce Jiang <jiangxd@embedinfo.com>
    Signed-off-by: Max Liao <liaops@embedinfo.com>

--- common/bootlogo/Android_Copy.mk
+++ common/bootlogo/Android_Copy.mk
@@ -6,6 +6,9 @@ LOCAL_PATH := device/atmel/common/bootlogo
 #
 $(shell mkdir -p root/system)
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/initlogo.rle:root/system/initlogo.rle \
-        $(LOCAL_PATH)/initlogo_pda4.rle:root/system/initlogo_pda4.rle \
-        $(LOCAL_PATH)/initlogo_pda7.rle:root/system/initlogo_pda7.rle
+	$(LOCAL_PATH)/wvga.rle:root/system/wvga.rle \
+        $(LOCAL_PATH)/hvga.rle:root/system/hvga.rle
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/init.wvga.rc:root/init.wvga.rc \
+        $(LOCAL_PATH)/init.hvga.rc:root/init.hvga.rc
