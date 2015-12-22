b2e402dd48f9f0ae60e2d923d7f8521fd064f253
Royce Jiang <jiangxd@embest-tech.com>
2015-12-22 16:25:48 +0800

    config: update wifi firmware
    Signed-off-by: Royce Jiang <jiangxd@embest-tech.com>

--- common/firmware/Android_Copy.mk
+++ common/firmware/Android_Copy.mk
@@ -8,5 +8,7 @@ $(shell mkdir -p root/lib/firmware/atmel/)
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/wilc1000_ap_fw.bin:root/lib/firmware/atmel/wilc1000_ap_fw.bin \
         $(LOCAL_PATH)/wilc1000_p2p_fw.bin:root/lib/firmware/atmel/wilc1000_p2p_fw.bin \
-        $(LOCAL_PATH)/wilc1000_fw.bin:root/lib/firmware/atmel/wilc1000_fw.bin
+        $(LOCAL_PATH)/wilc1000_fw.bin:root/lib/firmware/atmel/wilc1000_fw.bin \
+        $(LOCAL_PATH)/wilc1002_firmware.bin:root/lib/firmware/atmel/wilc1002_firmware.bin \
+        $(LOCAL_PATH)/wilc1003_firmware.bin:root/lib/firmware/atmel/wilc1003_firmware.bin
 
